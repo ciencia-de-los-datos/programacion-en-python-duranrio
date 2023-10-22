149f74e8c37e1fa5221d644034eb8d67ace60ccd
“duranr15” <daniel@MacBook-Air-de-Daniel.local>
2023-10-22 12:43:38 -0500

    laboratorio 1

--- preguntas.py
+++ preguntas.py
@@ -14,6 +14,7 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 def pregunta_01():
+    
     """
     Retorne la suma de la segunda columna.
 
@@ -21,7 +22,21 @@ def pregunta_01():
     214
 
     """
-    return
+    import csv
+    
+    sum_col_2 = 0
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            item_2 = int(line[1])
+            sum_col_2 +=item_2
+        
+
+    
+    return sum_col_2
+
 
 
 def pregunta_02():
@@ -39,7 +54,21 @@ def pregunta_02():
     ]
 
     """
-    return
+    import csv
+    
+    cant_letter = {}
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            cant_letter[line[0]]=cant_letter.get(line[0],0) + 1
+            
+    
+    rta =   sorted(cant_letter.items())
+            
+    return rta
+    
 
 
 def pregunta_03():
@@ -57,7 +86,20 @@ def pregunta_03():
     ]
 
     """
-    return
+    import csv
+    
+    dic = {}
+    item_col_2 = 0
+    
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv, delimiter='\t')
+        
+        for line in file:
+            item_col_2 = int(line[1])
+            dic[line[0]]= dic.get(line[0],0) + item_col_2
+
+    rta = sorted(dic.items())
+    return rta
 
 
 def pregunta_04():
@@ -82,7 +124,21 @@ def pregunta_04():
     ]
 
     """
-    return
+    import csv
+    
+    col_3 = {}
+    
+    
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            date = line[2]
+            month = (date.split('-'))[1]
+            col_3[month] = col_3.get(month,0) +1
+    
+    rta = sorted(col_3.items())
+    return   rta 
 
 
 def pregunta_05():
@@ -100,7 +156,39 @@ def pregunta_05():
     ]
 
     """
-    return
+    import csv
+    
+    max_min = {}
+    
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        
+        for line in file:
+            col_1 = line[0]
+            col_2 = int(line[1])
+            
+            
+            if col_1 not in max_min.keys():
+                
+                max_min[col_1] = [col_2,col_2]
+            
+            if col_2 > max_min[col_1][0] :
+                max_min[col_1][0] = col_2
+            
+            if col_2 < max_min[col_1][1] :
+                max_min[col_1][1] = col_2
+            
+    
+            
+        
+    max_min = dict(sorted(max_min.items()))
+    
+    list_max_min = [(letra, num1, num2) for letra, (num1, num2) in list(max_min.items())]
+
+    
+     
+    return list_max_min
 
 
 def pregunta_06():
@@ -125,7 +213,43 @@ def pregunta_06():
     ]
 
     """
-    return
+    import csv
+    
+    lista = []
+    dic_max_min = {}
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_4 = line[4]
+            items=col_4.split(',')
+            lista.append(items)
+            
+        
+        for line_2 in lista:
+            for item in line_2:
+                lista_2=item.split(':')
+                
+                key = lista_2[0]
+                value = int(lista_2[1])
+                if key not in dic_max_min.keys():
+                    dic_max_min[key] = [value,value]
+                
+                if value < dic_max_min[key][0]:
+                    dic_max_min[key][0] = value
+                
+                if value >  dic_max_min[key][1]:
+                    dic_max_min[key][1]=value
+        
+        dic_max_min = dict(sorted(dic_max_min.items()))     
+        
+        list_max_min = [(a,b,c) for (a,[b,c])  in list(dic_max_min.items())]
+        
+            
+            
+            
+    return list_max_min
 
 
 def pregunta_07():
@@ -149,7 +273,42 @@ def pregunta_07():
     ]
 
     """
-    return
+    
+    
+    import csv
+    
+    
+    dic_order = {}
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_0 = line[0]
+            col_1 = int(line[1])
+            
+            key = col_1
+            value = col_0
+            
+            if key not in dic_order.keys():
+                dic_order[key]=[value]
+            
+            else:
+                dic_order[key].append(value)
+
+            
+           
+        
+        dic_order = dict(sorted(dic_order.items()))
+        list_dic_order = list(dic_order.items())
+        
+        
+        
+            
+            
+            
+    return list_dic_order
+
 
 
 def pregunta_08():
@@ -174,7 +333,41 @@ def pregunta_08():
     ]
 
     """
-    return
+    import csv
+    
+    
+    dic_order = {}
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_0 = line[0]
+            col_1 = int(line[1])
+            
+            key = col_1
+            value = col_0
+            
+            if key not in dic_order.keys():
+                dic_order[key]=[value]
+            
+            elif value not in dic_order[key]:
+                dic_order[key].append(value)
+            
+            dic_order[key].sort()
+
+            
+           
+        
+        dic_order = dict(sorted(dic_order.items()))
+        list_dic_order = list(dic_order.items())
+        
+        
+        
+            
+            
+            
+    return list_dic_order
 
 
 def pregunta_09():
@@ -197,7 +390,38 @@ def pregunta_09():
     }
 
     """
-    return
+    import csv
+    
+    lista = []
+    dict_count = {}
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_4 = line[4]
+            items=col_4.split(',')
+            lista.append(items)
+            
+        
+        for line_2 in lista:
+            for item in line_2:
+                lista_2=item.split(':')
+                
+                key = lista_2[0]
+                
+                if key not in dict_count.keys():
+                    dict_count[key] = 1
+                else:
+                    dict_count[key] += 1
+                    
+                
+        
+        dict_count = dict(sorted(dict_count.items()))     
+        
+        
+        
+    return dict_count
 
 
 def pregunta_10():
@@ -218,7 +442,25 @@ def pregunta_10():
 
 
     """
-    return
+    import csv
+    
+    lista = []
+    
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_1, col_4, col_5 = line[0],line[3],line[4]
+            
+            col_4 = len(col_4.split(','))
+            col_5 = len(col_5.split(','))
+            
+            item = (col_1,col_4,col_5)
+            lista.append(item)
+            
+            
+    return lista
 
 
 def pregunta_11():
@@ -239,7 +481,31 @@ def pregunta_11():
 
 
     """
-    return
+    import csv
+    
+    
+    dict_items = {}
+    
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_2, col_4 = line[1],line[3]
+            
+            col_4=col_4.split(',')
+            
+            for key in col_4:
+                if key not in dict_items.keys():
+                    dict_items[key]= int(col_2)
+                
+                else:
+                    dict_items[key] += int(col_2)
+        
+        
+    dict_items = dict(sorted(dict_items.items()))       
+            
+    return dict_items
 
 
 def pregunta_12():
@@ -257,4 +523,30 @@ def pregunta_12():
     }
 
     """
-    return
+    import csv
+    
+    
+    dict_items = {}    
+    
+        
+    with open('data.csv') as file_csv:
+        file = csv.reader(file_csv,delimiter='\t')
+        
+        for line in file:
+            col_1, col_5 = line[0],line[4]
+            
+            col_5 = col_5.split(',')
+            item_sum = 0
+            for item in col_5:
+                item=item.split(':')
+                item_sum += int(item[1])
+            
+            if col_1 not in dict_items.keys():
+                dict_items[col_1] = item_sum
+            else:
+                dict_items[col_1] += item_sum
+    
+    
+    dict_items = dict(sorted(dict_items.items()))  
+    
+    return dict_items
